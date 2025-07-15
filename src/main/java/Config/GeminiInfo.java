7d976a7d8af9734c827ff706d32981b6e511ee06
Daniel Umberto <danielumberto51@gmail.com>
2025-07-15 13:25:05 -0300

    feat(config): Update environment variables

---
+++ src/main/java/Config/GeminiInfo.java
@@ -0,0 +1,15 @@
+package Config;
+
+public class GeminiInfo {
+
+    private String key = "KEY_IA";
+    private String link = "LINK_IA";
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getLink() {
+        return link;
+    }
+}
